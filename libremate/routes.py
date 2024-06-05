23a83b4376671be5e1e5689708363b6fbf7bec83
Amory <108595086+amorychambers@users.noreply.github.com>
2024-06-05 20:01:56 +0100

    create form to register new user

--- libremate/routes.py
+++ libremate/routes.py
@@ -7,3 +7,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")

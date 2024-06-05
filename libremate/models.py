3e2c7f61c3941840c0b7226d53f709df57abd9f4
Amory <108595086+amorychambers@users.noreply.github.com>
2024-06-05 18:54:29 +0100

    create models for new database

---
+++ libremate/models.py
@@ -0,0 +1,38 @@
+from libremate import db
+
+class Reader(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.username
+
+
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    genre_name = db.Column(db.String(20), nullable=False)
+    genre_owner = db.Column(db.String(15), db.ForeignKey("reader.username"), nullable=False)
+    books = db.relationship("Book", backref="genre", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.genre_name
+    
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String, nullable=False)
+    author_name = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False)
+    favourite = db.Column(db.Boolean, nullable=False)
+    review = db.Column(db.Text)
+    isbn = db.Column(db.Integer)
+    created_on = db.Column(db.Date, nullable=False)
+    book_genre = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    book_owner = db.Column(db.String(15), db.ForeignKey("reader.username"), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return f"{self.book_name} - {self.author_name}"
---
+++ libremate/routes.py
@@ -0,0 +1,4 @@
+from flask import render_template, request, redirect, url_for, flash, session
+from libremate import app, db
+from libremate.models import Reader, Genre, Book
+from werkzeug.security import generate_password_hash, check_password_hash

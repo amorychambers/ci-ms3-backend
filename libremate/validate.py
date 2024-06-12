a7e1d5dcc823a686511229e6dfade91e1295cc70
Amory <108595086+amorychambers@users.noreply.github.com>
2024-06-12 16:40:24 +0100

    add async function to await request for cover page to resolve

--- libremate/validate.py
+++ libremate/validate.py
@@ -1,7 +1,7 @@
 import requests
 
-def check_cover(i):
-    cover = requests.get(f'https://covers.openlibrary.org/b/isbn/{i}-L.jpg').status_code
+async def check_cover(i):
+    cover = await requests.get(f'https://covers.openlibrary.org/b/isbn/{i}-L.jpg').status_code
     if cover == "200":
         return i
     else:

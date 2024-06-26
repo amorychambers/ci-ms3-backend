22221293c040132deda0a845e9e57b9c6719c19d
Amory <108595086+amorychambers@users.noreply.github.com>
2024-06-26 20:28:44 +0100

    add conditional to community route accounting for crash caused when no books are public

--- libremate/routes.py
+++ libremate/routes.py
@@ -125,10 +125,14 @@ def add_book():
 @app.route("/community/<page>")
 def community(page):
     books = list(db.session.query(Book).order_by(Book.created_on.desc()).join(Reader).filter(Reader.private == False).all())
-    page_numbers = (math.ceil(len(books)/3))
-    groups = [books[i:i+3] for i in range(0, len(books), 3)]
-    current_group = groups[(int(page)-1)]
-    session['page'] = int(page)
+    if len(books) == 0:
+        page_numbers = 1
+        current_group = []
+    else:
+        page_numbers = (math.ceil(len(books)/3))
+        groups = [books[i:i+3] for i in range(0, len(books), 3)]
+        current_group = groups[(int(page)-1)]
+        session['page'] = int(page)
     return render_template("community.html", books=current_group, page_numbers=page_numbers)
 
 

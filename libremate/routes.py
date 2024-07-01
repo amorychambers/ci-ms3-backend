3ba533d2888bf897496551c00370330481697f97
Amory <108595086+amorychambers@users.noreply.github.com>
2024-07-01 16:41:00 +0100

    add for loop to my_library_sort route to only display a row for a book status with books assigned

--- libremate/routes.py
+++ libremate/routes.py
@@ -80,7 +80,12 @@ def my_library():
 def my_library_sort(sort):
     genres = Genre.query.order_by(Genre.genre_name).filter(Genre.genre_owner == session["user"]).all()
     books = Book.query.order_by(sort).filter(Book.book_owner == session["user"]).all()
-    return render_template("my_library.html", genres=genres, books=books, sort=sort)
+    status_options = ["complete", "plan-to-read", "dropped"]
+    statuses = []
+    for status in status_options:
+        if len(Book.query.filter(Book.status == status and Book.book_owner == session["user"]).all()) > 1:
+            statuses.append(status)
+    return render_template("my_library.html", genres=genres, books=books, sort=sort, statuses=statuses)
 
             
 

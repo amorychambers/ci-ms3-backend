1fc9ac3c108c355ed47b5171886ae294ea30e617
Amory <108595086+amorychambers@users.noreply.github.com>
2024-07-03 22:53:12 +0100

    docs: add docstrings to cover.js

--- libremate/static/js/cover.js
+++ libremate/static/js/cover.js
@@ -1,3 +1,7 @@
+/**
+ * This file contains the functions related to the feature that lets users search for a cover image for their books by the ISBN.
+ * It accesses data through the Open Library Covers API and gives the user the opportunity to confirm or reject the cover.
+ */
 let checkButton = document.getElementById("cover-check");
 let userConfirmation = document.getElementById("user-confirmation");
 let confirmButton = document.getElementById("confirm");
@@ -9,6 +13,10 @@ checkButton.addEventListener('click', checkCover);
 confirmButton.addEventListener('click', confirmCover);
 rejectButton.addEventListener('click', rejectCover);
 
+/**
+ * Takes the ISBN value input by the user and inserts it into the API link.
+ * Displays the retrived cover image on the page and disables the button that calls the API until user confirms or rejects cover.
+ */
 function checkCover(event){
     event.preventDefault();
     event.stopPropagation();
@@ -21,6 +29,10 @@ function checkCover(event){
     userConfirmation.insertAdjacentElement('afterbegin', cover);
 }
 
+/**
+ * Adds a confirmation message to show cover has been accepted after selecting Confirm button.
+ * Preserves the ISBN input to be saved in the database, but allows user to search and try again. 
+ */
 function confirmCover(event){
     event.preventDefault();
     event.stopPropagation();
@@ -35,6 +47,10 @@ function confirmCover(event){
     userConfirmation.insertAdjacentElement("beforebegin", message);
 }
 
+/**
+ * Resets the value of the ISBN input box to 0, which can be saved in the database to indicate no cover image retrieved and to use standardised site cover.
+ * Re-enables button that calls API so user can search again.
+ */
 function rejectCover(event){
     event.preventDefault();
     event.stopPropagation();
@@ -45,5 +61,3 @@ function rejectCover(event){
     input.value = 0;
 
 }
-
-console.log("smello")

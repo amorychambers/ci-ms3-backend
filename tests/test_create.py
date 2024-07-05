3a6524fcb83d9c56ec652e2f4078634d82727dce
Amory <108595086+amorychambers@users.noreply.github.com>
2024-07-05 22:35:42 +0100

    test: add tests for creating new user and new genre

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,29 +1,72 @@
-import flask_unittest, flask.globals
+import flask.globals
 from tests.conftest import TestCase
-from libremate import create_app, db
-from libremate.models.models import Reader
+from libremate import db
+from libremate.models.models import Reader, Genre
+from werkzeug.security import generate_password_hash
 
 
 class TestNewUser(TestCase):
 
     def test_confirm_existing(self, client):
-        self.assertTrue(db.session.query(Reader).filter(Reader.username == "amory").one_or_none())
+        self.assertTrue(db.session.query(Reader).filter(
+            Reader.username == "amory").one_or_none())
 
     def test_create_user(self, client):
-        response = client.post("/register", data={"username": "testuser", "new-password": "testpass", "private": False})
-        self.assertTrue(db.session.query(Reader).filter(Reader.username == "testuser").one_or_none())
+        response = client.post(
+            "/register", data={"username": "testuser", "new-password": "testpass", "private": False})
+        self.assertTrue(db.session.query(Reader).filter(
+            Reader.username == "testuser").one_or_none())
         self.assertIn("user", flask.globals.session)
         self.assertLocationHeader(response, "/my_library")
 
     def test_existing_username(self, client):
-        response = client.post("/register", data={"username": "amory", "new-password": "testpass", "private": False})
+        response = client.post(
+            "/register", data={"username": "amory", "new-password": "testpass", "private": False})
         self.assertNotIn("user", flask.globals.session)
         self.assertLocationHeader(response, "/register")
 
     @classmethod
     def tearDownClass(cls) -> None:
-        testuser = db.session.query(Reader).filter(Reader.username == "testuser").one()
+        testuser = db.session.query(Reader).filter(
+            Reader.username == "testuser").one()
         db.session.delete(testuser)
         db.session.commit()
         return super().tearDownClass()
-    
+
+
+class TestNewGenre(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        testuser = Reader(username="testuser",
+                          password=generate_password_hash("testpass"), private=False)
+        existing_genre = Genre(
+            genre_name="existing_genre", genre_owner="testuser")
+        db.session.add(testuser)
+        db.session.add(existing_genre)
+        db.session.commit()
+        return super().setUpClass()
+
+    def test_new_genre(self, client):
+        client.post(
+            "/sign_in", data={"username": "testuser", "password": "testpass"})
+        response = client.post(
+            "/add_genre", data={"genre_name": "testgenre", "genre_owner": flask.globals.session["user"]})
+        self.assertLocationHeader(response, "/my_library")
+        self.assertTrue(db.session.query(Genre).filter(
+            Genre.genre_owner == "testuser", Genre.genre_name == "testgenre").one_or_none())
+
+    def test_existing_genre(self, client):
+        client.post(
+            "/sign_in", data={"username": "testuser", "password": "testpass"})
+        response = client.post(
+            "/add_genre", data={"genre_name": "existing_genre", "genre_owner": flask.globals.session["user"]})
+        self.assertLocationHeader(response, "/add_genre")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        testuser = db.session.query(Reader).filter(
+            Reader.username == "testuser").one()
+        db.session.delete(testuser)
+        db.session.commit()
+        return super().tearDownClass()

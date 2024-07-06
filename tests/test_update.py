514cf631a8c3c99fdf32e642c5b2bc4ad66ec2e1
Amory <108595086+amorychambers@users.noreply.github.com>
2024-07-06 19:49:38 +0100

    test: add tests for edit_book and edit_genre routes

--- tests/test_update.py
+++ tests/test_update.py
@@ -21,7 +21,7 @@ class TestUpdateBook(TestCase):
         testuser = Reader(username="testuser",
                           password=generate_password_hash("testpass"), private=False)
         genre = Genre(
-            genre_name="misc", genre_owner="testuser")
+            genre_name="testgenre", genre_owner="testuser")
         book = Book(
             book_title="testbook", author_name="testauthor", status="plan-to-read", favourite=False, review="", isbn=None, created_on="07/05/24 22:57:21", book_genre=203, book_owner="testuser"
         )
@@ -44,6 +44,43 @@ class TestUpdateBook(TestCase):
         updated_testbook = db.session.query(Book).filter(
             Book.id == testbook.id).one()
         self.assertEqual(updated_testbook.status, "complete")
+        self.assertEqual(updated_testbook.favourite, True)
+        self.assertIn("not so bad", updated_testbook.review)
+
+    def test_edit_genre(self, client):
+        """
+        Confirm that /edit_genre route updates database entry
+        """
+        client.post(
+            "/sign_in", data={"username": "testuser", "password": "testpass"})
+        genre = db.session.query(Genre).filter(
+            Genre.genre_name == "testgenre", Genre.genre_owner == "testuser").one()
+        client.post("/edit_genre/{}".format(genre.id), data={'genre_name': 'Horror'})
+        updated_genre = db.session.query(Genre).filter(Genre.id == genre.id).one()
+        self.assertEqual(updated_genre.genre_name, "Horror")
+
+    def test_edit_genre(self, client):
+        """
+        Confirm that /edit_genre route does not update database entry if genre name already exists
+        """
+        client.post(
+            "/sign_in", data={"username": "testuser", "password": "testpass"})
+        genre = db.session.query(Genre).filter(
+            Genre.genre_name == "testgenre", Genre.genre_owner == "testuser").one()
+        # Following code snippet to test for flash messages on the page taken from suggestion of stackoverflow user Leo Skhrnkv, linked in readme credits
+        response = client.post("/edit_genre/{}".format(genre.id), data={'genre_name': 'testgenre'}, follow_redirects=True)
+        self.assertIn("That genre already exists in your library!", response.get_data(as_text=True))
+        
+    def test_edit_auth_fail(self, client):
+        """
+        Confirm that /edit_genre route does not update database entry when user not logged into session
+        """
+        genre = db.session.query(Genre).filter(
+            Genre.genre_name == "testgenre", Genre.genre_owner == "testuser").one()
+        client.post("/edit_genre/{}".format(genre.id), data={'genre_name': 'Horse Books'})
+        updated_genre = db.session.query(Genre).filter(Genre.id == genre.id).one()
+        self.assertNotEqual(updated_genre.genre_name, "Horse Books")
+
 
     @classmethod
     def tearDownClass(cls) -> None:

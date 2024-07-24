337b92cf8991e2b55e4e5a09850df41a109822b4
Amory <108595086+amorychambers@users.noreply.github.com>
2024-07-24 16:22:42 +0100

    feat: make community search function case insensitive using sqlalchemy func.lower method

--- libremate/shared/routes.py
+++ libremate/shared/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, session, request
+from sqlalchemy import func
 from libremate import db
 from libremate.models.models import Reader, Book
 import math
@@ -9,9 +10,9 @@ shared = Blueprint("shared", __name__)
 @shared.route("/community/<page>", methods=["GET", "POST"])
 def community(page):
     if request.method == "POST":
-        search = request.form.get("search_term")
+        search = request.form.get("search_term").lower()
         books = list(db.session.query(Book).order_by(Book.created_on.desc()).filter(
-            Book.book_title.contains(search) | Book.author_name.contains(search)).join(
+            func.lower(Book.book_title).contains(search) | func.lower(Book.author_name).contains(search)).join(
             Reader).filter(Reader.private == False).all())
     else:
         search = None
